e174399dbad4c963d29d586db098fcef55860ba8
Monique <monique.jones712@gmail.com>
2019-09-11 18:32:57 -0500

    part 3 completed cheese/index cheese.category.name still not functioning

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -29,8 +29,9 @@ public class CategoryController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model){
-        model.addAttribute(new Category());
+
         model.addAttribute("title", "Add Category");
+        model.addAttribute(new Category());
 
         return "category/add";
     }
